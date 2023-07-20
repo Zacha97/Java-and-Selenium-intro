f4bc52f6025a99f67e61aced1cdd358d0a357a76
Zacha <grzegorz.zachariasz1997@gmail.com>
2023-07-20 20:46:31 +0200

    Updated FirstTest.java and pom.xml.

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class FirstTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    WebDriver driver;
+    @BeforeEach
+    public void driverSetup(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1275,730)); //ustawianie width oraz height przeglądarki
+    }
+
+    @AfterEach
+    public void driverQuit(){
+  //      driver.quit();
+  //        driver.close();
+    }
+
+    @Test
+    public void getMethod(){
+        driver.get("http://www.google.pl");
+    }
+
+    @Test
+    public void navigate(){
+        driver.navigate().to("https://www.google.com");
+        driver.navigate().to("https://www.nasa.gov");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
 }
